69d244b53ae397fc2d2904c4b7839deff97b8533
aaguilarv <aaguilarv@loggro.com>
2025-03-13 20:43:21 -0500

    Querys

--- queries/global_ammount_order_status.sql
+++ queries/global_ammount_order_status.sql
@@ -1,3 +1,11 @@
 -- TODO: Esta consulta devolverá una tabla con dos columnas: estado_pedido y
 -- Cantidad. La primera contendrá las diferentes clases de estado de los pedidos,
 -- y la segunda mostrará el total de cada uno.
+
+SELECT
+	order_status AS order_status,
+	COUNT(order_status) AS Ammount
+FROM
+	olist_orders
+GROUP BY
+	order_status;
